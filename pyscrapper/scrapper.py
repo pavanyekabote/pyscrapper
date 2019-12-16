4ec449ddc3db8dea2c96c4f91d826089ee2ae2d1
Pavan Yekabote <yekabotep@gmail.com>
2019-12-16 18:19:05 +0530

    Refractored : BlockHolder class
    - Renamed BlockHolder to PyScrapper
    - Added utilities file which contains all utility functions.
    - Complete transformation of code base.

--- pyscrapper/scrapper.py
+++ pyscrapper/scrapper.py
@@ -1,244 +1,167 @@
 from logging import Logger
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from pkg_resources import  resource_filename
+from pkg_resources import resource_filename
 import warnings
-
+from .utilities import get_attr, parse_tags, normalize_parsed_dict
 warnings.filterwarnings("ignore", category=UserWarning, module=webdriver.__name__)
 
-DRIVER_PATH =  resource_filename(__name__,"resources/phantomjs")
+DRIVER_PATH = resource_filename(__name__, "resources/phantomjs")
 driver = webdriver.PhantomJS(DRIVER_PATH)
+
+
+headers = { 'Accept':'*/*',
+    'Accept-Encoding':'gzip, deflate, sdch',
+    'Accept-Language':'en-US,en;q=0.8',
+    'Cache-Control':'max-age=0',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+}
+
+for key, value in enumerate(headers):
+    capability_key = 'phantomjs.page.customHeaders.{}'.format(key)
+    webdriver.DesiredCapabilities.PHANTOMJS[capability_key] = value
+
 logger = Logger("scrapper")
 
-def getHtmlContent(url):
+def get_html_content(url):
     global driver
     driver.get(url)
     html = driver.find_element_by_tag_name('body').get_attribute("innerHTML")
-    content = ""
-    try:
-        content = html;
-    except:
-        content = ""
-    # f = open("file.html", "w")
-    # f.write(html)
-    # f.close()
-    soup = BeautifulSoup(content, "html.parser")
+    soup = BeautifulSoup(html, "html.parser")
     return soup
 
-class BlockHolder:
-    LIST_ITEM = 'listItem'
-    DATA = 'data'
-    SELECTOR = 'selector'
-    ATTR = 'attr'
-    EQ = "eq"
-    WHICH = "which"
-    const_keys = [LIST_ITEM, DATA, SELECTOR, ATTR, EQ, WHICH]
 
-    def __init__(self, html, config, isFirst=False, isList=False, name=''):
-        self.is_parent = False
-        self.children = []
-        self.obj_parent = None
-        self.isList = isList
-        print(name, "HTML TYPE : ", type(html), "Config ==> ", config)
-        #         if(type(html) == ResultSet):
-        #             print("Resultset : ",html)
+class PyScrapper:
+
+    __LIST_ITEM, __DATA = 'listItem', "data"
+    __SELECTOR, __ATTR = "selector", "attr"
+    __EQ = "eq"
+
+    CONST_KEYS = {__LIST_ITEM: True, __DATA: True, __SELECTOR: True,
+                  __ATTR: True, __EQ: True }
+
+    def __init__(self, html, config, is_list = False, name=''):
+        self.is_list = is_list
+        self.result = {}
         self.config = config
-        self.isFirst = isFirst
-        self.obj_name = name
-        self.fObj = {}
+        self.key_name = name
+        self.html, self.can_parse_next = self.__check_and_join_html(html)
+        self.element_index = self.__get_index_to_select(config)
+        # print("Config ", self.config, " Contenx" , self.can_parse_next)
+
+    @staticmethod
+    def __check_and_join_html(html):
+        """ This method helps to make a list of html as one html
+        and checks if the current html can further be parsed. Returns html: BeautifulSoup, can_further_be_parsed : boolean"""
+        can_be_parsed_next = False
         if type(html) == list:
             html = ' '.join([str(lis) for lis in html])
-        if html != None and len(html) != 0 and (type(html) == BeautifulSoup or type(html) == str):
-            self.html = html if type(html) == BeautifulSoup else BeautifulSoup(html, "html.parser")
-            self.__parse_config()
 
-    def name(self, name):
-        self.obj_name = name
-        return self
+        if html is not None and len(html) != 0 and (type(html) == BeautifulSoup or type(html) == str):
+            html = html if type(html) == BeautifulSoup else BeautifulSoup(html, "html.parser")
+            can_be_parsed_next = True
+        return html, can_be_parsed_next
 
-    def parent(self, parent):
-        self.obj_parent = parent
-        return self
+    def __parse_configuration(self):
+        """ This method parses and grabs elements from html, creates relevant object(s) as per the configuration """
 
-    def __parse_config(self):
-        mWhich = self.__get_attr(self.config, self.WHICH)
-        mWhich = mWhich - 1 if mWhich != None and type(mWhich) == int else None
         if type(self.config) == dict:
+            """ check if the configuration is dict """
 
-            mhtml = self.html
+            if get_attr(self.config, self.__LIST_ITEM) is not None:
+                result_list = self.__parse_list(self.html)
+                self.result = result_list
+
+            if get_attr(self.config, self.__SELECTOR) is not None:
+                self.html = parse_tags(self.html, get_attr(self.config, self.__SELECTOR))
+                if not self.is_list:
+                    self.result = self.html[0] if len(self.html) > 0 else self.html
+                else:
+                        self.result = self.html
+
+
+            if get_attr(self.config, self.__ATTR) is not None:
+                object_key = get_attr(self.config, self.__ATTR)
+                if not self.is_list:
+                    self.html = self.html[0] if len(self.html) > 0 else self.html
+                    self.html = get_attr(self.html, object_key)
+                else:
+                    self.html = [get_attr(obj, object_key) for obj in self.html] if len(self.html) > 0 else self.html
+                        # map(lambda obj: get_attr(obj, object_key), self.html) if len(self.html) > 0 else self.html
+                self.result = self.html
+
+            if get_attr(self.config, self.__EQ) is not None:
+                if self.is_list:
+                    self.result = self.result[self.element_index]
+
+            # Parse for all keys
             keys = self.config.keys()
-            if not self.isFirst:
-                if self.LIST_ITEM in keys:
+            for key in keys:
+                if get_attr(self.CONST_KEYS, key) is None:
+                    key_block = get_attr(self.config, key)
+                    self.result[key] = self.__get_result_from_non_const_keys(key, key_block)
 
-                    self.isParent = True
-                    self.isList = True
-                    print("MWHICH ",mWhich)
-                    # If no data block i.e, no keys available in data, nothing can be selected
-                    if self.__get_attr(self.config, self.DATA) == None:
-                        self.fObj = [item for item in self.__parse_tags(mhtml, self.__get_attr(self.config, self.LIST_ITEM))]
-                        if mWhich is not None:
-                            final_which= self.__get_attr(self.fObj, mWhich)
-                            self.fObj = final_which if  final_which != None else ''
-                        return
-
-
-                    childHtml = self.__parse_tags(mhtml, self.__get_attr(self.config, self.LIST_ITEM))
-                    if childHtml != '' or len(childHtml) > 0:
-                        obs = [{child_key: child_config} for child_key, child_config in self.config[self.DATA].items() ]
-                        self.children = {child_key:
-                                             BlockHolder(childHtml, child_config, name=child_key, isList=True).name(
-                                                 child_key).parent(
-                                                 self).parsed_data() \
-                                         for child_key, child_config in self.config[self.DATA].items() if child_key not in self.const_keys}
-                        print("CHILDREN : ", self.children)
-                        self.fObj = self.normalize(self.children)
-                        if mWhich is not None:
-                            print("After children ... ", mWhich, self.__get_attr(self.config, self.WHICH))
-                            final_which= self.__get_attr(self.fObj, mWhich)
-                            self.fObj = final_which if  final_which != None else ''
-                if self.SELECTOR in keys:
-                    tag = self.config[self.SELECTOR]
-                    print("Selector : ", tag)
-                    mhtml = self.__parse_tags(mhtml, tag)
-                    if not self.isList:
-                        self.fObj = mhtml[0] if len(mhtml) > 0 else mhtml
-                    else:
-                        self.fObj = mhtml
-
-                if self.ATTR in keys:
-                    k = self.config[self.ATTR]
-                    if not self.isList:
-                        mhtml = mhtml[0] if len(mhtml) > 0 else mhtml
-                        mhtml = self.__get_attr(mhtml, k)
-                    else:
-                        mhtml = [self.__get_attr(mhtm, k) for mhtm in mhtml] if len(mhtml) > 0 else mhtml
-                    self.fObj = mhtml
-
-                for key in keys:
-                    if key not in self.const_keys:
-                        data_block = self.__get_attr(self.config, key)
-                        # If key's value is a string which means it is a selector, then just parse the selector and  set data.
-                        if type(data_block) == str:
-                            parsed_data = self.__parse_tags(mhtml, data_block)
-                            if self.ATTR not in keys and len(parsed_data) > 0:
-                                self.fObj[key] = parsed_data[0].text if len(parsed_data) == 1 else \
-                                    [str(p.text) for p in parsed_data]
-
-
-                        elif type(data_block) == dict:
-                            print(self.isList, "DBLOCK ", data_block)
-                            block = BlockHolder(mhtml, data_block, name=key, isList=self.isList).name(key).parent(self)
-                            self.fObj[key] = block.parsed_data()
         elif type(self.config) == str:
-            parsed_data = self.__parse_tags(self.html, self.config)
-            self.fObj = parsed_data[0].text if len(parsed_data) == 1 else \
-                [str(p.text) for p in parsed_data]
-            if mWhich is not None:
-                final_which = self.__get_attr(self.fObj, mWhich)
-                self.fObj = final_which if final_which != None else ''
+            self.html = parse_tags(self.html, self.config)
+            res = self.html[0].text if len(self.html) == 1  else \
+                    [str(x.text) for x in self.html]
+            self.result = res
 
-    def parsed_data(self):
-        return self.fObj
+    def __get_result_from_non_const_keys(self, key, key_block):
+        """ This method works on those keys which are not a part of CONST keys  """
+        res = ''
+        if type(key_block) == str:
+            self.html = parse_tags(self.html, key_block)
+            if get_attr(self.config, self.__ATTR) is None and len(self.html) > 0:
+                res = self.html[0].text if len(self.html) == 1 else \
+                      [str(p.text) for p in self.html]
+        elif type(key_block) == dict:
+            res = PyScrapper(self.html, key_block, name=key, is_list=self.is_list).get_scrapped_config()
+        return res
 
-    def normalize(self, indict):
-        arr = []
-        index = 0
-        size = max([len(vals) for vals in indict.values()])
-        # size = len(next(iter(indict.values())))
-        while index < size:
-            mObj = {}
-            for k, v in indict.items():
+    def __get_index_to_select(self, config) -> object:
+        """ Check if there is an eq(helps to fetch particular index from list) option actively available and return
+        it, else return None """
+
+        eq_idx, eq = get_attr(config, self.__EQ), None
+        if eq_idx is not None:
+            if type(eq_idx) == str:
                 try:
-                    mObj[k] = str(v[index]).strip()
+                    eq_idx = int(eq_idx)
                 except:
-                    mObj[k] = ''
-            arr.append(mObj)
-            index += 1
-        return arr
+                    pass
+            eq = eq_idx if type(eq_idx) == int else None
+        return eq
 
-    def __get_attr(self, obj, key):
-        #         print(obj, key)
-        try:
-            return obj[key]
-        except:
-            return None
+    def __parse_list(self, html):
+        """ This method parses html and forms a list of configuration mirrored objects present in data block"""
 
-    # Parses html selector tags to soup searchable tags
-    # Eg :  #source-person >  div .name
-    # The above eg is parsed to [ soup.select('#source-person'), soup.find_all('div', {'class' : "name"}) ] which will be
-    # serially executed on each one's result
+        self.is_list = True
+        # Check if DATA field is available or not
+        data_value = get_attr(self.config, self.__DATA, none_if_empty=True)
+        tag_parsed_html = parse_tags(html, get_attr(self.config, self.__LIST_ITEM))
+        if data_value is None:
+            result = [str(item.text).strip() for item in tag_parsed_html]
+            return result
 
-    def __parse_tags(self, mhtml, tags):
-        if mhtml == None:
-            return []
-        html = mhtml
-        childs = tags.split(">")
-        #         print("Length : ",len(childs), childs)
-        # For each of tag group : [ video #source-person, div .name ]
-        for child in childs:
-            mClass, mId, mSelector = [], None, None
+        sub_blocks_dict = {}
+        if tag_parsed_html != '' or len(tag_parsed_html) >0:
+            sub_blocks_dict = { key : PyScrapper(tag_parsed_html, conf, name=key, is_list=self.is_list).get_scrapped_config() \
+                                for key, conf in get_attr(self.config, self.__DATA).items() \
+                                if get_attr(self.CONST_KEYS, key) is None }
+            sub_blocks_dict = normalize_parsed_dict(sub_blocks_dict)
 
-            # For each of tag in [video, #source-person]
-            for label in child.split():
-                #                 print("Label : ",label)
-                # If tag starts with . => consider it as class
-                if label.startswith("."):
-                    mClass.append(label)
+        return sub_blocks_dict
 
-                # If tag startsWith # => consider it as (unique) id
-                elif label.startswith("#"):
-                    if mId is not None:
-                        raise Exception("Cannot have multiple id's at block " + str(child) + " in " + str(tags))
-                    mId = label
-
-                # Else consider the tag as a selector i.e, an html tag
-                else:
-                    if mSelector is not None:
-                        raise Exception("Cannot have multiple selectors at block " + str(child) + " in " + str(tags))
-                    mSelector = label
-            if (self.obj_name == 'name'):
-                print(" VARS: ", mId, mSelector, mClass, html)
-
-            #             print("Parsing for ",(mId, mSelector, mClass))
-            # End of parsing label
-            # if id is not none; then find directly with id. No need of any other selectors
-            if mId is not None:
-                if type(html) == BeautifulSoup:
-                    html = [htm for htm in html.select(mId)]
-                else:
-                    html = [item for htm in html for item in htm.select(mId)]
+    def get_scrapped_config(self):
+        if self.can_parse_next:
+            self.__parse_configuration()
+        if self.element_index is not None and self.is_list:
+            if type(self.result) == list:
+                print("CONFIG : ",self.config, " IDX : ",self.element_index, self.result)
+                self.result = get_attr(self.result, self.element_index)
+        return self.result
 
 
-            # If selector is not none and class is present then
-            # html.find_all('div', {'class' : 'name person'})
-            elif mSelector != None and len(mClass) > 0:
-                attrs = {"class": ' '.join([cls[1:] for cls in mClass])}
-                if type(html) == BeautifulSoup:
-                    html = [htm for htm in html.find_all(mSelector, attrs)]
-                else:
-                    html = [item for htm in html for item in htm.find_all(mSelector, attrs)]
-
-            # If mSelector is not available and only mClasses are available, then select all elements with those classes
-            # html.select('.name.person')
-            elif mSelector == None and len(mClass) > 0:
-                clsparam = ''.join(mClass)
-                if type(html) == BeautifulSoup:
-                    html = [htm for htm in html.select(clsparam)]
-                else:
-                    html = [item for htm in html for item in htm.select(clsparam)]
-            elif mSelector != None and len(mClass) == 0 and mId == None:
-                if type(html) == BeautifulSoup:
-                    html = [htm for htm in html.find_all(mSelector)]
-                else:
-                    html = [item for htm in html for item in htm.find_all(mSelector)]
-        if self.obj_name == 'name':
-            print("Returning HTML ", html)
-        return html
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __str__(self):
-        return str(self.obj_name) + " => " + str(self.config) + " ==> " + str(self.fObj)
 
 
---
+++ pyscrapper/utilities.py
@@ -0,0 +1,118 @@
+from bs4 import BeautifulSoup
+
+# Get Attribute from object Or Get Index from array etc...
+def get_attr_with_err(src_object, index, none_if_empty=False, obj_if_empty=False):
+    result, is_error = None, False
+    try:
+        result = src_object[index]
+        if type(result) == list and none_if_empty:
+            if len(result) == 0 or len(result) < index:
+                result = None
+    except:
+        is_error = True
+
+    if obj_if_empty:
+        if type(result) == list and result == None:
+            result = []
+        if type(result) == dict and result is None:
+            result = {}
+    return result, is_error
+
+
+def get_attr(src_object, index, none_if_empty=False, obj_if_empty=False):
+    res, _ = get_attr_with_err(src_object, index, none_if_empty, obj_if_empty)
+    return res
+
+
+def parse_tags(html, structured_tag):
+    """ Parses structured tags like div .select > #id and fetches its values from html """
+    if html is None:
+        return []
+    # Split with > character
+    tag_blocks = structured_tag.split(">")
+
+    for tag_block in tag_blocks:
+        selector_tuple = parse_selector(tag_block)
+        html = fetch_html_with(*selector_tuple, html)
+        # print("Selector ", *selector_tuple, html)
+    return html
+
+def parse_selector(selector):
+    """Parses a block of selectors like div .name #tag to class=.name, selector=div and id=#tag.
+    Returns (selector, id, class[]) """
+    m_class, m_id, m_selector = [], None, None
+    if selector is not None and type(selector) == str:
+        selector_labels = selector.split()
+        for label in selector_labels:
+            if label.startswith("."):
+                m_class.append(label)
+            elif label.startswith("#"):
+                if m_id is not None:
+                    raise ValueError("Multiple id's are declared in block "+str(selector))
+                m_id = label
+            else:
+                if m_selector is not None:
+                    raise ValueError("Multiple selectors are declared in block "+str(selector))
+                m_selector = label
+    return m_selector, m_id, m_class
+
+
+def fetch_html_with(m_selector, m_id, m_class, html):
+    if m_id is not None:
+        html = listify_soup(html, select=True, tupled_attrs=(m_id))
+
+    elif m_selector is not None and len(m_class) > 0:
+        attrs = {'class' : ' '.join([cls[1:] for cls in m_class])}
+        html = listify_soup(html,tupled_attrs=(m_selector, attrs))
+
+    elif m_selector is None and len(m_class) > 0:
+        cls_param = ''.join(m_class)
+        html = listify_soup(html, select=True, tupled_attrs=(cls_param))
+
+    elif m_selector is not None and len(m_class) == 0 and m_id is None:
+        html = listify_soup(html, tupled_attrs=(m_selector))
+    return html
+
+
+def listify_soup(soup_object_list, select = False, tupled_attrs = ()):
+    result = []
+    is_tuple = type(tupled_attrs) == tuple
+    source_arr =  soup_object_list#get_selector_result(soup_object_list, is_select=select, attrs=tupled_attrs)
+    if type(soup_object_list) == BeautifulSoup:
+        result = [soup for soup in get_selector_result(source_arr, is_select=select, attrs=tupled_attrs)]
+    else:
+        result = [soup for soup_object in source_arr for soup in \
+                  get_selector_result(soup_object, is_select=select, attrs=tupled_attrs)]
+    return result
+
+
+def get_selector_result(soup, is_select=False, attrs = ()):
+    is_tuple = type(attrs) == tuple
+    res = []
+    if is_tuple:
+        if is_select:
+            res = soup.select(*attrs)
+        if not is_select:
+            res = soup.find_all(*attrs)
+    else:
+        if is_select:
+            res = soup.select(attrs)
+        else:
+            res = soup.find_all(attrs)
+    return res
+
+
+def normalize_parsed_dict(pdict : dict) -> list:
+    arr, index = [], 0
+    size = max([len(vals) for vals in pdict.values()])
+    while index < size:
+        mObj = {}
+        for k, v in pdict.items():
+            try:
+                mObj[k] = str(v[index]).strip()
+            except:
+                mObj[k] = ''
+        arr.append(mObj)
+        index += 1
+    return arr
+
